4b8d709ea77457e5199ea9359b7d71d4ffe12eca
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-10-29 18:31:15 +0300

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-    const houses = await req.storage.getFirst3();
+    const houses = await req.storage.getTopListings();
     res.render('home',{houses});
 });
 
